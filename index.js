e48741e2c5097d7f56c6ef63627a6875a29fc9c4
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-06-05 20:27:29 +0000

    add pop up winner

--- index.js
+++ index.js
@@ -1,112 +1,141 @@
 const CONFIG_AI = {
-    side: "",
-    delay: 100
-}
+  side: "",
+  delay: 100,
+};
 
 function App() {
-    const gameController = GameController()
-    const renderController = RenderController()
-    const aiControl = AiControl()
+  const gameController = GameController();
+  const renderController = RenderController();
+  const aiControl = AiControl();
 
-    let aiIsLance = false
-    const AI_ENABLE = true
+  let aiIsLance = false;
+  const AI_ENABLE = true;
 
-    const setupMap = () => {
-        const lances = [
-            // { x: 1, y: 0 },
-            // { x: 0, y: 0 },
-            // { x: 0, y: 2 },
-            // { x: 2, y: 0 },
-            // { x: 2, y: 2 },
-        ]
+  const setupMap = () => {
+    const lances = [
+      // { x: 1, y: 0 },
+      // { x: 0, y: 0 },
+      // { x: 0, y: 2 },
+      // { x: 2, y: 0 },
+      // { x: 2, y: 2 },
+    ];
 
-        lances.forEach(({ x, y }) => {
-            const _house = document.querySelector(`.table .house[data-position-x="${x}"][data-position-y="${y}"]`)
+    lances.forEach(({ x, y }) => {
+      const _house = document.querySelector(
+        `.table .house[data-position-x="${x}"][data-position-y="${y}"]`
+      );
 
-            if (!_house) { return }
+      if (!_house) {
+        return;
+      }
 
-            playerLance(x, y, _house, true)
-        })
+      playerLance(x, y, _house, true);
+    });
+  };
+
+  const aiLance = async () => {
+    if (!game.running) {
+      return;
     }
 
-    const aiLance = async() => {
-        if (!game.running) { return }
-
-        if (!AI_ENABLE) { return }
-
-        aiIsLance = true
-
-        const resAI = await aiControl.newLance(gameController.game.map)
-
-        console.log(resAI)
-
-        const { x, y } = resAI
-
-        const _house = document.querySelector(`.table .house[data-position-x="${x}"][data-position-y="${y}"]`)
-
-        if (!_house) { return }
-
-        playerLance(x, y, _house, true)
-
-        aiIsLance = false
+    if (!AI_ENABLE) {
+      return;
     }
 
-    const playerLance = (x, y, _house, force = false) => {
-        if (!game.running) { return }
+    aiIsLance = true;
 
-        if (!force && aiIsLance) { return }
+    const resAI = await aiControl.newLance(gameController.game.map);
 
-        gameController.lance(x, y)
-        renderController.newLance(_house, x, y)
+    console.log(resAI);
+
+    const { x, y } = resAI;
+
+    const _house = document.querySelector(
+      `.table .house[data-position-x="${x}"][data-position-y="${y}"]`
+    );
+
+    if (!_house) {
+      return;
     }
 
-    document.querySelectorAll(".table .house").forEach(_house => {
-        const x = _house.getAttribute("data-position-x") || ""
-        const y = _house.getAttribute("data-position-y") || ""
+    playerLance(x, y, _house, true);
 
-        if (!x || !y) { return }
+    aiIsLance = false;
+  };
 
-        _house.addEventListener("click", () => {
-            if (!game.running) { return }
+  const playerLance = (x, y, _house, force = false) => {
+    if (!game.running) {
+      return;
+    }
 
-            playerLance(x, y, _house)
-        })
-    })
+    if (!force && aiIsLance) {
+      return;
+    }
 
-    document.querySelector('[name="bt-new-game"]').addEventListener("click", () => {
-        gameController.newGame()
-        renderController.resetGame()
-    })
+    gameController.lance(x, y);
+    renderController.newLance(_house, x, y);
+  };
 
-    gameController.subscribeObserver({
-        code: "$game/new-game",
-        observerFunction: ({ data }) => {
-            renderController.resetGame(data.side)
+  document.querySelectorAll(".table .house").forEach((_house) => {
+    const x = _house.getAttribute("data-position-x") || "";
+    const y = _house.getAttribute("data-position-y") || "";
 
-            const playerAI = data.players.find(player => player.code == "ai")
+    if (!x || !y) {
+      return;
+    }
 
-            aiControl.setup({...CONFIG_AI, side: playerAI.side })
-        }
-    })
+    _house.addEventListener("click", () => {
+      if (!game.running) {
+        return;
+      }
 
-    gameController.subscribeObserver({
-        code: "$game/new-lance",
-        observerFunction: ({ data }) => {
-            renderController.newLance(data.position.x, data.position.y, data.currentSide, data.oldSide)
+      playerLance(x, y, _house);
+    });
+  });
 
-            data.player.code == "player" && aiLance()
-        }
-    })
-    gameController.subscribeObserver({
-        code: "$game/end-game",
-        observerFunction: ({ data }) => {
-            renderController.gameEnd(data.winner)
-        }
-    })
+  document
+    .querySelector('[name="bt-new-game"]')
+    .addEventListener("click", () => {
+      gameController.newGame();
+      renderController.resetGame();
+    });
 
-    gameController.newGame()
-    renderController.initComponents(gameController.game.players.map(pl => pl.username).reverse())
-    setupMap()
+  gameController.subscribeObserver({
+    code: "$game/new-game",
+    observerFunction: ({ data }) => {
+      renderController.resetGame(data.side);
+
+      const playerAI = data.players.find((player) => player.code == "ai");
+
+      aiControl.setup({ ...CONFIG_AI, side: playerAI.side });
+    },
+  });
+
+  gameController.subscribeObserver({
+    code: "$game/new-lance",
+    observerFunction: ({ data }) => {
+      renderController.newLance(
+        data.position.x,
+        data.position.y,
+        data.currentSide,
+        data.oldSide
+      );
+
+      data.player.code == "player" && aiLance();
+    },
+  });
+  gameController.subscribeObserver({
+    code: "$game/end-game",
+    observerFunction: ({ data }) => {
+      renderController.gameEnd(data.winner, data.player);
+    },
+  });
+
+  gameController.newGame();
+  renderController.initComponents(
+    gameController.game.players.map((pl) => pl.username).reverse()
+  );
+  setupMap();
 }
 
-window.onload = App
+window.onload = App;
--- scripts/render-controller.js
+++ scripts/render-controller.js
@@ -1,76 +1,90 @@
 function RenderController() {
-    const players = {
-        o: document.querySelector('.player-info[data-player-side="o"]'),
-        x: document.querySelector('.player-info[data-player-side="x"]'),
+  const players = {
+    o: document.querySelector('.player-info[data-player-side="o"]'),
+    x: document.querySelector('.player-info[data-player-side="x"]'),
+  };
+
+  const gameContent = document.querySelector(".table-content");
+  const table = document.querySelector(".table");
+  const popUp = document.querySelector(".winner-pop-up");
+
+  const ICON_CLASS = {
+    x: "x-lg",
+    o: "circle",
+  };
+
+  const initComponents = (players) => {
+    document
+      .querySelectorAll(".player-username")
+      .forEach((el, i) => (el.innerHTML = (players && players[i]) || "Guest_"));
+
+    toggleSide();
+  };
+
+  const resetGame = (side) => {
+    document
+      .querySelectorAll(".table .house")
+      .forEach((_house) => (_house.innerHTML = ""));
+
+    players["o"].classList.toggle("winner", false);
+    players["x"].classList.toggle("winner", false);
+    players["o"].classList.toggle("draw", false);
+    players["x"].classList.toggle("draw", false);
+
+    gameContent.classList.toggle("enable", true);
+    gameContent.classList.toggle("disable", false);
+
+    document.querySelectorAll(`.house.disable`).forEach((house) => {
+      house.classList.toggle("disable", false);
+    });
+
+    toggleSide(side);
+    popUp.classList.toggle("active", false);
+  };
+
+  const gameEnd = (side, player) => {
+    gameContent.classList.toggle("enable", false);
+    gameContent.classList.toggle("disable", true);
+
+    if (side == "-") {
+      players["o"].classList.add("draw");
+      players["x"].classList.add("draw");
+
+      return;
     }
 
-    const gameContent = document.querySelector(".table-content")
-    const table = document.querySelector(".table")
+    popUp.querySelector(".player-winner-name").innerHTML = player.username;
 
-    const ICON_CLASS = {
-        x: "x-lg",
-        o: "circle"
+    popUp.classList.toggle("active", true);
+
+    players[side].classList.add("winner");
+  };
+
+  const newLance = (x, y, currentSide, oldSide) => {
+    const house = document.querySelector(
+      `.house[data-position-x="${x}"][data-position-y="${y}"]`
+    );
+
+    if (!house) {
+      return currentSide;
     }
 
-    const initComponents = (players) => {
-        document.querySelectorAll(".player-username").forEach((el, i) => el.innerHTML = players && players[i] || "Guest_")
+    house.innerHTML = `<i class="icon bi-${ICON_CLASS[oldSide]} ${oldSide}"></i>`;
 
-        toggleSide()
-    }
+    house.classList.add("disable");
 
-    const resetGame = (side) => {
-        document.querySelectorAll(".table .house").forEach(_house => _house.innerHTML = "")
+    toggleSide(currentSide);
+  };
 
-        players["o"].classList.toggle("winner", false)
-        players["x"].classList.toggle("winner", false)
-        players["o"].classList.toggle("draw", false)
-        players["x"].classList.toggle("draw", false)
+  const toggleSide = (side = "") => {
+    players.o.classList.toggle("this-side", side == "o");
+    players.x.classList.toggle("this-side", side == "x");
+  };
 
-        gameContent.classList.toggle("enable", true)
-        gameContent.classList.toggle("disable", false)
-
-        document.querySelectorAll(`.house.disable`).forEach(house => {
-            house.classList.toggle("disable", false)
-        })
-
-        toggleSide(side)
-    }
-
-    const gameEnd = (side) => {
-        gameContent.classList.toggle("enable", false)
-        gameContent.classList.toggle("disable", true)
-
-        if (side == "-") {
-            players["o"].classList.add("draw")
-            players["x"].classList.add("draw")
-
-            return
-        }
-
-        players[side].classList.add("winner")
-    }
-
-    const newLance = (x, y, currentSide, oldSide) => {
-        const house = document.querySelector(`.house[data-position-x="${x}"][data-position-y="${y}"]`)
-
-        if (!house) { return currentSide }
-
-        house.innerHTML = `<i class="icon bi-${ICON_CLASS[oldSide]} ${oldSide}"></i>`
-
-        house.classList.add("disable")
-
-        toggleSide(currentSide)
-    }
-
-    const toggleSide = (side = "") => {
-        players.o.classList.toggle("this-side", side == "o")
-        players.x.classList.toggle("this-side", side == "x")
-    }
-
-    return {
-        newLance,
-        initComponents,
-        resetGame,
-        gameEnd
-    }
+  return {
+    newLance,
+    initComponents,
+    resetGame,
+    gameEnd,
+  };
 }
